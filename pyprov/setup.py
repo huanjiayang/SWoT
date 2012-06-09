ddfc377ed850d0c7e7bdcfd6b751fcf6e796f8e8
Huanjia Yang <huanjiayang@hotmail.com>
2012-06-09 23:03:18 +0100

    first register/biuld for pyprov

--- pyprov/setup.py
+++ pyprov/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='provpy',
-    version='1.1.3',
+    name='pyprov',
+    version='1.0.0',
     author='Huanjia Yang',
     author_email='huanjiayang@hotmail.com',
-    packages=['provpy', 'provpy.model','provpy.model.examples','provpy.model.test'],
+    packages=['pyprov', 'pyprov.model','pyprov.model.examples','pyprov.model.test'],
     scripts=[],
-    url='http://github.com/trungdong/w3-prov/tree/master/provpy',
+    url='http://github.com/huanjiayang',
     license='LICENSE.txt',
-    description='A Python implementation of PROV-DM data model.',
+    description='A Python implementation of PROV data model.',
     long_description=open('README').read(),
     install_requires=[],
 )
