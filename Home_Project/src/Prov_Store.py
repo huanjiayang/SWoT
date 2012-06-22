4a328fe4e5bf6eb83f3e02d4365a91941716b013
Huanjia Yang <huanjiayang@hotmail.com>
2012-06-22 15:58:16 +0100

    changes during weekly meeting

--- Home_Project/src/Prov_Store.py
+++ Home_Project/src/Prov_Store.py
@@ -12,17 +12,26 @@ from rdflib.term import URIRef
 from rdflib import plugin
 from rdflib.store import Store, NO_STORE, VALID_STORE
 
-
+import datetime
+from pyprov.model.type import *
+from pyprov.model.relation import *
+from pyprov.model.bundle import *
 
 
 
 # Define Namespace
 hs = Namespace("http://homesensor.com#")
+prov = Namespace("http://www.w3.org/ns/prov-dm/")
 
+S_Network = hs['SensorNetwork']
+
+sensornetworkURI = 'uri:uuid:sensnetworkayodele001'
+
+createtime = 'datetime...'
 
 #
 S_Network = URIRef("http://homesensor.com/")
-Readings = URIRef("http://homesensor.com/Activity/")
+Readings = hs['Reading']
 Communication = URIRef("http://homesensor.com/Activity/")
 Discovery = URIRef("http://homesensor.com/Activity/")
 S_Node = URIRef("http://homesensor.com/S_node/")
@@ -32,6 +41,10 @@ graph=Graph(store='Sleepycat',identifier='test')
 graph.open("provfolder", create=True)
 
 # Addition of triples to graph
+
+
+
+
 
 graph.add((S_Network, hs['wasGeneratedBy'], Literal('Network Organization')))
 graph.add((S_Network, hs['wasAssociatedWith'], Literal('Communication')))
@@ -46,6 +59,32 @@ graph.add((S_Node, hs['wasAssociatedWith'], Literal('Communication')))
 graph.add((Readings, hs['startedAtTime'], Literal("2011-11-16T16:06:00")))
 graph.add((Readings, hs['endedAtTime'],  Literal("2012-06-19T01:52:02Z")))
 graph.add((Sensor,  hs['actedOnbehalfOf'], Literal('Sensor Node')))
+
+
+class WSNPROV:
+    
+    def __init__(self):
+        self.provgraph = PROVContainer()
+        
+    def addNetwork(self,sensornetworkURI,createtime,person):
+#        graph.add((sensornetworkURI, RDF.type, S_Network))
+#        graph.add((sensornetworkURI, dcterms['create'], createtime))
+#        graph.add((sensornetworkURI, prov['wasGeneratedBy'], person))
+        
+        e0 = Agent(sensornetworkURI,attribute={})
+        self.provgraph.add(e0)
+    
+    def convertDataToPROV(self,rawdata):
+        pass
+    
+    def createWGB(self,para1,para2,para3):
+        wGB = wasGeneratedBy(para1,para2,para3)
+        return wGB
+    
+    def createEntity(self):
+        pass
+    
+    
 
 
 # commit graph
