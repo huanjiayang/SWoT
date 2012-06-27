5c4fce8da2860ad59eb5d5b066462c6709550c00
AYODELE-M.AKINGBULU <a.m.akingbulu-11@student.lboro.ac.uk>
2012-06-27 19:23:59 +0100

    Modification to Last Code for functions(27-06-2012)

--- Home_Project/src/Prov_Store.py
+++ Home_Project/src/Prov_Store.py
@@ -6,8 +6,7 @@ from rdflib.term import Literal
 from rdflib.term import URIRef
 from rdflib import plugin
 from rdflib.store import Store, NO_STORE, VALID_STORE
-
-
+from datetime import date, time, datetime
 
 import datetime
 from pyprov.model.type import *
@@ -23,6 +22,19 @@ prov = Namespace("http://www.w3.org/ns/prov-dm/")
 S_Network = hs['SensorNetwork']
 
 sensornetworkURI = 'uri:uuid:sensnetworkayodele001'
+sensornode1URI = 'uri:uuid:sensornode01'
+sensornode2URI = 'uri:uuid:sensornode02'
+sensornode3URI = 'uri:uuid:sensornode03'
+sensortemp1URI = 'uri:uuid:sensorTEMP01'
+sensortemp2URI = 'uri:uuid:sensorTEMP02'
+sensortemp3URI = 'uri:uuid:sensorTEMP03'
+sensorlight1URI = 'uri:uuid:sensorLIGHT01'
+sensorlight2URI = 'uri:uuid:sensorLIGHT02'
+sensorlight3URI = 'uri:uuid:sensorLIGHT03'
+sensorHum1URI = 'uri:uuid:sensorHUM01'
+sensorHum1URI = 'uri:uuid:sensorHUM02'
+sensorHum1URI = 'uri:uuid:sensorHUM03'
+
 
 createtime = 'datetime...'
 
@@ -56,56 +68,73 @@ class WSNPROV:
 #        graph.add((sensornetworkURI, dcterms['create'], createtime))
 #        graph.add((sensornetworkURI, prov['wasGeneratedBy'], person))
         
-        e0 = Agent(sensornetworkURI,attribute={})
+        a0 = Agent(sensornetworkURI,attribute={})
+        e0 = Entity()
+        g0 = Activity()
+        d0=wasDerivedFrom(e0, activity=a0,generation=g0,attributes=None)
+        f0=wasGeneratedBy(e0,g0,time=None,attributes=None)
+        
         self.provgraph.add(e0)
     
     def convertDataToPROV(self,rawdata):
         pass
     
     
-    def createWGB(self,Agent,Activity,Entity,identifier=None):
+    def createWGB(self,Agent=None,Activity,Entity,identifier=None,time=None,attributes=None):
         """Define WGB relation with self as context"""
-        wGB = wasGeneratedBy(Agent,Activity,Entity)
+        wGB = wasGeneratedBy(Activity,Entity,identifier,time,attributes)
+        
+        attrdict = {}
         return wGB
    
-    def createWAW(self):
+    def createWAW(self,identifier=None,Agent,Activity,attributes):
         """Define WAW relation with self as context"""
-        wAW = wasAssociatedWith(Agent,Activity,Entity)
+        wAW = wasAssociatedWith(Agent,Activity,Entity,identifier)
         return wAW
     
-    def createWSBA(self):
+    def createWSBA(self,identifier=None,started,starter,attributes):
         """Define WSBA relation with self as context"""
-        wSBA = wasStartedByActivity(Agent,Activity,Entity)
+        wSBA = wasStartedByActivity(Activity,identifier,started,starter)
+        a1 = starter
+        a2 = started
         return wSBA
     
-    def createWSB(self):
+    def createWSB(self,identifier=None,Activity=None,Entity=None,time=None,attributes=None):
         """Define WSB relation with self as context"""
-        wSB = wasStartedBy(Agent,Activity,Entity)
+        wSB = wasStartedBy(Agent,Activity,Entity,identifier,time,attributes)
         return wSB
     
-    def createdAOBO(self):
+    def createdAOBO(self,Agent2,Agent1,Activity,identifier,attributes):
         """Define AOBO relation with self as context"""
-        aOBO = actedOnBehalfOf(Agent,Activity,Entity) 
+        aOBO = actedOnBehalfOf(Agent2,Agent1,Activity,identifier,attributes) 
+        ag1 = Agent1
+        ag2 = Agent2
         return aOBO
     
-    def createdWAT(self):
+    def createdWAT(self,Entity,Agent,identifier=None,attributes):
         """Define WAT relation with self as context"""
-        wAT = wasAttributedTo(Agent,Activity,Entity)
+        wAT = wasAttributedTo(Agent,Entity,identifier=None,attributes)
         return wAT
     
-    def createdWDF(self):
+    def createdWDF(self,Activity,identifier=None,Entity1,Entity2):
         """Define WDF relation with self as context"""
-        wDF = wasDerivedFrom(Agent,Activity,Entity)
+        wDF = wasDerivedFrom(Agent,Activity,Entity1,Entity2,identifier)
+        e1 = Entity1
+        e2 = Entity2
         return wDF
     
     
-    def createEntity(self,Attribute=[]):
+    def createEntity(self,attributes=None,identifier=None):
+        
         pass
     
-    def createActivity(self):
+    def createActivity(self, identifier=None, starttime, endtime, attributes=None):
+        
+        starttime = starttime
+        endtime = endtime
         pass
     
-    def createAgent(self):
+    def createAgent(self,identifier=None,attributes=None):
         pass
     
     def _toRDF(self):
@@ -117,13 +146,20 @@ class WSNPROV:
     
     def Prov_toRDF(self):
         
-        tripledict = {'sensornetworkURI' : {'wasGeneratedBy' : 'S_Network'},
-                       
-                      'subject02' : {'pred02_01' : 'obj02_01'}}
+        tripledict = {'sensornetworkURI' : {'wasGeneratedBy' : 'S_Network',
+                                            'wasAssociatedWith' : 'S_Network',
+                                            'actedOnbehalfOf' : 'S_Network'},
+                                             
+                      'sensornode1URI' : {'pred02_01' : 'obj02_01'},
+                      'sensortemp1URI' : {'pred02_01' : 'obj02_01'},
+                      'sensorlight1URI' : {'pred02_01' : 'obj02_01'}}
+        
         return tripledict
     
     
     def RDF_toStore(self):
+        
+        
         
         
         
