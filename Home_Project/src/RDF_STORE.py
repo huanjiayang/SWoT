f27a9d92ab031a54e2ca5d8348630aa96a10145e
AYODELE-M.AKINGBULU <a.m.akingbulu-11@student.lboro.ac.uk>
2012-07-10 17:27:49 +0100

    latest changes to RDF_STORE.py commit in meeting

--- Home_Project/src/RDF_STORE.py
+++ Home_Project/src/RDF_STORE.py
@@ -4,21 +4,35 @@ Created on 6 Jul 2012
 @author: AYODELE-M.AKINGBULU
 '''
 
+import rdflib
+import rdfextras
+from rdflib.graph import Graph, ConjunctiveGraph
+from rdflib.namespace import Namespace
+from rdflib.term import Literal
+from rdflib.term import URIRef
+from rdflib import plugin
+from rdflib.store import Store, NO_STORE, VALID_STORE
+from datetime import date, time, datetime
+
 class RDFSTORE:
+    ''' Initialization method that creates indexes for triplestore, each index holds a permutation
+    of the three triples stored in the graph for easy iteration '''
     def __init__(self):
-        self._subject = {}
-        self._predicate = {}
-        self._object = {}
+        self._spo = {}
+        self._pos = {}
+        self._osp = {}
         
         
         
     def add(self, (subject,predicate,obj)):
-        self._addToStore(self._subject, (subject, predicate, obj))
-        self._addToStore(self._predicate, (predicate, obj, subject))
-        self._addToStore(self._object, (obj, subject, predicate))
+        ''' Add method permutates subject,object and predicate to match ordering of each index '''
+        self._addToIndex(self._spo, (subject, predicate, obj))
+        self._addToIndex(self._pos, (predicate, obj, subject))
+        self._addToIndex(self._osp, (obj, subject, predicate))
         
     
     def RDF_toStore(self,store,sub,pred,obj):
+        ''' Add terms to index creating, creating a dictionary and set if terms are not present in index '''
         if sub not in store: 
             store[sub] = {pred:set([obj])}
         else:
@@ -26,3 +40,31 @@ class RDFSTORE:
                 store[sub][pred] = set([obj])
             else: store[sub][pred].add(obj)
             
+
+#Load triples into graph
+    def load(self, filename, reader):
+        mystore = plugin.get('Sleepycat', Store)('mystore')
+        mystore.open("ay_folder", create=False)
+        mystore = open("ay_folder", "wb")
+        for line in file:
+            file.write(line + '\n')
+        reader = mystore.reader(file)
+        for sub, pred, obj in mystore:
+            sub = unicode(sub, "UTF-8")
+            pred = unicode(pred, "UTF-8")
+            obj = unicode(obj, "UTF-8")
+            self.add((sub, pred, obj))
+            mystore.close()
+            
+#Save triples in graph
+    def save(self, filename):
+        mystore = open(filename="ay_folder", "wb")
+        writer = mystore.writer(mystore)
+        for sub, pred, obj in self.triples((None, None, None)):
+            writer.writerow([sub.encode("UTF-8"), pred.encode("UTF-8"), \
+            obj.encode("UTF-8")])
+            mystore.close()         
+        
+        
+        
+        
