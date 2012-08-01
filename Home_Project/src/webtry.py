46e56acc3bff25702551a0094ee763ba6814fd14
Huanjia Yang <huanjiayang@hotmail.com>
2012-08-01 16:33:26 +0100

    changes during meeting 01/08/2012

--- Home_Project/src/webtry.py
+++ Home_Project/src/webtry.py
@@ -22,48 +22,61 @@ import commands
 import logging
 import sys
 
+sensor_graph=Mystore('mystore', 'mystore')
 
-labels = file('sensor_labels.txt', 'r').readlines() #Load graph labels from a file
+
+#labels = file('sensor_labels.txt', 'r').readlines() #Load graph labels from a file
 render = web.template.render('templates/')
 
 
-def get_type(self,sensorid,node_id):
-        for line in labels:
-            node_id, label, fieldtype = line.split(',', 2)
-            if int(id) == int(sensorid):
-                return fieldtype.rstrip('\r\n')
+#def get_type(self,sensorid,node_id):
+#        for line in labels:
+#            node_id, label, fieldtype = line.split(',', 2)
+#            if int(id) == int(sensorid):
+#                return fieldtype.rstrip('\r\n')
             
 #Get Label reads in sensor names, determines the type name of the sensor
 #to generate graphs on page properly
-def get_label(sensorid, node_id, fieldtype):
-    for line in labels:
-        node_id, label, fieldtype = line.split(',', 2)
-        print 'line: '
-        print str(line)
-        if int(id) == int(sensorid):
-            return labels
+#def get_label(sensorid, node_id, fieldtype):
+#    for line in labels:
+#        node_id, label, fieldtype = line.split(',', 2)
+#        print 'line: '
+#        print str(line)
+#        if int(id) == int(sensorid):
+#            return labels
         
 
 
         
 class HS_Network:
-    def GET(self, HS_Network, store):
-        store = plugin.get('mystore', rdflib.store.Store)('mystore')
-        store.open("mystore", create=False)
-        store = open(store, "rb")
-        cur = store.cursor() 
+    def GET(self):
+        
+        #cur = store.cursor() 
         
     #query triples in store
-        cur.execute('select ?pred ?obj where {<%s> ?pred ?obj .}' % id)
-       
-        return json.dumps(cur.fetchone())
+        #cur.execute('select ?pred ?obj where {<%s> ?pred ?obj .}' % id)
+        returndict = {'whats_in_the_store' : '',
+                      'subject' : '',
+                      'predicate' : '',
+                      'object:)' : ''}
+        returnlist = []
+        for s,p,o in sensor_graph.store:
+            returndict['whats_in_the_store'] = returndict['whats_in_the_store'] + str(s) + str(p) + str(o)
+            returndict['subject'] = str(s)
+            returndict['predicate'] = str(p)
+            returndict['object:)'] = str(o)
+        
+        returnlist.append(returndict)
+            
+        web.header('Content-Type', 'application/json')
+        return json.dumps(returndict)
     
     
 
 if __name__ == "__main__":
 #Links specific classes to URLs on the web server
     urls = (
-    '/homesensor.com/', 'HS_Network', #Links URL homesensor.com to HS_Network Class
+    '/homesensorcom/', 'HS_Network', #Links URL homesensor.com to HS_Network Class
     '/sensor','sensor'
         )
     app = web.application(urls, globals()) #Run the web server.
