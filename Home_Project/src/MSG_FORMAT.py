50ea8db25b0dea3af707f6ec91b3c5238c334cc7
AYODELE-M.AKINGBULU <a.m.akingbulu-11@student.lboro.ac.uk>
2012-08-30 15:39:19 +0100

    uncommented part of program for MSG_FORMAT

--- Home_Project/src/MSG_FORMAT.py
+++ Home_Project/src/MSG_FORMAT.py
@@ -32,25 +32,25 @@ from pyprov.model.bundle import *
 from Home_Sense_Model import *
 
 
-#
-#api = twitter.Api(consumer_key='your key here', consumer_secret='your key here', access_token_key='your key here', access_token_secret='your key here')
-#
-## Twitter username & password
-#twitterusername = "sense_AY"
-#twitterpassword = "sensorbizy_007"
-#
-#
-#def TwitterIt(u, p, message):
-#    api = twitter.Api(username=u, password=p)
-#    print u, p
-#    try:
-#        status = api.PostUpdate(message)
-#        print "%s just posted: %s" % (status.user.name, status.text)
-#    except UnicodeDecodeError:
-#        print "Your message could not be encoded.  Perhaps it contains non-ASCII characters? "
-#        print "Try explicitly specifying the encoding with the  it with the --encoding flag"
-#    except:
-#        print "Couldn't connect, check network, username and password!"
+
+api = twitter.Api(consumer_key='your key here', consumer_secret='your key here', access_token_key='your key here', access_token_secret='your key here')
+
+# Twitter username & password
+twitterusername = "sense_AY"
+twitterpassword = "sensorbizy_007"
+
+
+def TwitterIt(u, p, message):
+    api = twitter.Api(username=u, password=p)
+    print u, p
+    try:
+        status = api.PostUpdate(message)
+        print "%s just posted: %s" % (status.user.name, status.text)
+    except UnicodeDecodeError:
+        print "Your message could not be encoded.  Perhaps it contains non-ASCII characters? "
+        print "Try explicitly specifying the encoding with the  it with the --encoding flag"
+    except:
+        print "Couldn't connect, check network, username and password!"
 #
 #
 #
@@ -63,100 +63,100 @@ from Home_Sense_Model import *
 ###line = ser.readline()
 ##print data     # write a string
 ##ser.close()             # close port
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#        
-#    
-#
-#
-##ser = serial.Serial(0)  # open first serial port
-#try:
-#        ser = serial.Serial('COM12',115200,timeout=1,parity=serial.PARITY_NONE,
-#              stopbits=serial.STOPBITS_ONE,
-#              bytesize=serial.EIGHTBITS,
-#              )
-#        print ser.portstr 
-#        status = ser.read()
-#        #Post status to twitter
-#        api.PostUpdate(status.text)
-#        time.sleep(3600)  ##sleep for 3600 seconds
-##        ser.open()
-#except:
-#        print "Could not open serial port: ", sys.exc_info()[0]
-#        sys.exit(2)
-#
-#print ser.portstr       # check which port was really used
-##ser.write("hello")      # write a string
-#
-#while True:
-#    ser.flushInput()
-#    data = str(ser.read(100).strip());
-#    data = data.replace("\n","").replace("\r","")
-#    data = data.split(",")
-#    try:
-#        
-#        if len(data) > 0:
-#            data.append(('timestamp:', time.asctime()))
-#            #print 'Got:', data
-#  
-#        
-##        data = int("data",16)
-##        ADC_value = raw_value / 10
-##        temperature = (ADC_value - 500) / 10                               
-##        time.sleep(0.5)
-#        
-##        data = struct.unpack('>fff', data)
-##        data = struct.unpack('B', data)
-#
-#        print "data: ", data
-#        
-#        
-#      
-#
-##        print 'not blocked'
-#
-#
-# 
-#
-#
-#    except KeyboardInterrupt:
-#        break
-#
-#
-#ser.close()
-#
-#
-#
-#
-#
-#def sensor_detail(self,get_sample,sensor,Node):
-#    get_sample = self.get_temperature
-#    get_sample = self.get_humidity
-#    get_sample = self.get_Light
-#    values = get_sample()
-#    data.append(values)
-#    if len(data) > 0:
-#        data.pop(0)
-#    result = []
-#    for sensor in data[0]:
-#        result .append([])
-#    for sample in data:
-#        for sensor_id in range(len(sample)):
-#            value = sample[sensor_id]
-#            result[sensor_id].append(value)
-#            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
+
+#ser = serial.Serial(0)  # open first serial port
+try:
+        ser = serial.Serial('COM12',115200,timeout=1,parity=serial.PARITY_NONE,
+              stopbits=serial.STOPBITS_ONE,
+              bytesize=serial.EIGHTBITS,
+              )
+        print ser.portstr 
+        status = ser.read()
+        #Post status to twitter
+        api.PostUpdate(status.text)
+        time.sleep(3600)  ##sleep for 3600 seconds
+#        ser.open()
+except:
+        print "Could not open serial port: ", sys.exc_info()[0]
+        sys.exit(2)
+
+print ser.portstr       # check which port was really used
+#ser.write("hello")      # write a string
+
+while True:
+    ser.flushInput()
+    data = str(ser.read(100).strip());
+    data = data.replace("\n","").replace("\r","")
+    data = data.split(",")
+    try:
+        
+        if len(data) > 0:
+            data.append(('timestamp:', time.asctime()))
+            #print 'Got:', data
+  
+        
+#        data = int("data",16)
+#        ADC_value = raw_value / 10
+#        temperature = (ADC_value - 500) / 10                               
+#        time.sleep(0.5)
+        
+#        data = struct.unpack('>fff', data)
+#        data = struct.unpack('B', data)
+
+        print "data: ", data
+        
+        
+      
+
+#        print 'not blocked'
+
+
+ 
+
+
+    except KeyboardInterrupt:
+        break
+
+
+ser.close()
+
+
+
+
+
+def sensor_detail(self,get_sample,sensor,Node):
+    get_sample = self.get_temperature
+    get_sample = self.get_humidity
+    get_sample = self.get_Light
+    values = get_sample()
+    data.append(values)
+    if len(data) > 0:
+        data.pop(0)
+    result = []
+    for sensor in data[0]:
+        result .append([])
+    for sample in data:
+        for sensor_id in range(len(sample)):
+            value = sample[sensor_id]
+            result[sensor_id].append(value)
+            
             
 #---------------------Home Sensor Model Instance based on serial message received-------------------------------
 
@@ -181,7 +181,7 @@ Avt4 = Sensor_Reading_Activity(identifier=HS["SRA"], attributes=None, account=No
 
 #---------------------Function to check or add Home Sensor Model Instances--------------------------
 
-def sense_instance(sensor_dict, sensor_store):    
+def sense_instance(self,sensor_dict, sensor_store):    
     #sensor_dict = [13]
     found = False    
     for i in sensor_dict:
@@ -194,6 +194,7 @@ def sense_instance(sensor_dict, sensor_store):
                     break
             if (found == False):
                 print i, " not in store."
+                self.store.add(i)
                 # TODO: add to store
              
 
