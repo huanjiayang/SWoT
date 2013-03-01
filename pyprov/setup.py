389892ebeb7dafcb52f82277ac853c7c8a8ee552
Huanjia Yang <huanjiayang@hotmail.com>
2013-03-01 14:27:17 +0000

    Changes to update pyprov to 1.0.10

--- pyprov/setup.py
+++ pyprov/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='pyprov',
-    version='1.0.9',
+    version='1.0.10',
     author='Huanjia Yang',
     author_email='huanjiayang@hotmail.com',
     packages=['pyprov', 'pyprov.model','pyprov.model.examples','pyprov.model.test'],
     scripts=[],
     url='http://github.com/huanjiayang',
     license='LICENSE.txt',
-    description='A Python implementation of PROV data model.',
+    description='A Python implementation of W3C PROV data model.',
     long_description=open('README').read(),
     install_requires=[],
 )
