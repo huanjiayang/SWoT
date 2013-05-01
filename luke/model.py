6edbf0c14ac973366891b5482a332437aae25efe
LukeJia <lukegenius@163.com>
2013-05-01 12:14:51 +0100

    update of model

--- luke/model.py
+++ luke/model.py
@@ -61,6 +61,8 @@ class Measuring(Activity):
         
         Activity.__init__(self, indentifier=identifier, starttime=starttime, endtime=endtime, attributes=attributes, account=account)
         
+        self.indentifier = identifier
+        
     def _toRDF(self):
         Activity.to_RDF(self)
         self.rdftriples[self.identifier][rdf['type']] = MT['Measuring']
@@ -71,6 +73,24 @@ class Measuring(Activity):
         return self.rdftriples
 
 
+class Sand_and_Recieve(Activity):
+    
+    def __init__(self, identifier=None, starttime=None, endtime=None, attributes=None, account=None, sensor_name=None, sensor_id=None):
+        
+        Activity.__init__(self, indentifier=identifier, starttime=starttime, endtime=endtime, attributes=attributes, account=account)
+        
+        self.indentifier = identifier
+        
+    def _toRDF(self):
+        Activity.to_RDF(self)
+        self.rdftriples[self.identifier][rdf['type']] = MT['Sand_and_Recieve']
+        if self.starttime is not None:
+            self.rdftriples[self.identifier][MT['starttime']] = MT[self.starttime]
+        if self.endtime is not None:
+            self.rdftriples[self.identifier][MT['endtime']] = MT[self.endtime]
+        return self.rdftriples
+    
+    
 class Measured_value(Entity):
     
     def __init__(self, identifier=None, attributes=None, value=None, sensor_name=None, sensor_id=None,account=None):
@@ -83,4 +103,56 @@ class Measured_value(Entity):
         Entity._toRDF(self)
         self.rdftriples[self.identifier][rdf['type']] = MT['Measured_value']
         self.rdftriples[self.identifier][MT['value']] = Literal(self.value)
+        return self.rdftriples
+    
+
+class Sensor(Entity):
+    
+    def __init__(self, identifier=None, attributes=None, sensor_name=None, sensor_id=None, account=None):
+        
+        Entity.__init__(self, identifier=idetifier, attributes=attributes, account=account)
+        self.identifier = identifier
+        
+    def _toRDF(self):
+        Entity._toRDF(self)
+        self.rdftriples[self.identifier][rdf['type']] = MT['Sensor']
+        return self.rdftriples
+
+
+class Request(Entity):
+    
+    def __init__(self, identifier=None, attributes=None, account=None):
+        
+        Entity.__init__(self, identifier=idetifier, attributes=attributes, account=account)
+        self.identifier = identifier
+        
+    def _toRDF(self):
+        Entity._toRDF(self)
+        self.rdftriples[self.identifier][rdf['type']] = MT['request']
+        return self.rdftriples
+    
+    
+class Command(Entity):
+    
+    def __init__(self, identifier=None, attributes=None, account=None):
+        
+        Entity.__init__(self, identifier=idetifier, attributes=attributes, account=account)
+        self.identifier = identifier
+        
+    def _toRDF(self):
+        Entity._toRDF(self)
+        self.rdftriples[self.identifier][rdf['type']] = MT['Command']
+        return self.rdftriples
+    
+    
+class Respond(Entity):
+    
+    def __init__(self, identifier=None, attributes=None, account=None):
+        
+        Entity.__init__(self, identifier=idetifier, attributes=attributes, account=account)
+        self.identifier = identifier
+        
+    def _toRDF(self):
+        Entity._toRDF(self)
+        self.rdftriples[self.identifier][rdf['type']] = MT['Respond']
         return self.rdftriples
