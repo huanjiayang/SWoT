569ccc3a668ea52aa08526f111cad433dd82f683
jojojiang <jojo0622@yahoo.cn>
2013-05-25 20:52:04 +0100

    generate uuid and put it under the own namespace

--- jojo/rdf_store.py
+++ jojo/rdf_store.py
@@ -3,7 +3,6 @@ import uuid
 import rdflib
 from rdflib import URIRef,Literal, Namespace, Graph, ConjunctiveGraph, RDF
 
-
 ns_prov = Namespace('http://www.w3.org/ns/prov-dm/')
 ns_ebook = Namespace('http://jiaojiaojiang.com/ebook/')
 
@@ -19,26 +18,26 @@ store.bind('ebook',ns_ebook)
 #store.add((input,ns_prov:['wasGeneratedBy'],userinput))
 #store.add((result,ns_prov:['wasDerivedFrom'],input))
 
-result_id = 'urn:uuid:' + str(uuid.uuid1())
-store.add((result_id,RDF.type,ns_ebook['result']))
-store.add((result_id,ns_ebook['source'],Literal['output.txt']))
-store.add((result_id,ns_ebook['value'],Literal[result]))
+result_uuid = str(uuid.uuid1())
+store.add((ns_ebook[result_uuid],RDF.type,ns_ebook['result']))
+store.add((ns_ebook[result_uuid],ns_ebook['source'],Literal['output.txt']))
+store.add((ns_ebook[result_uuid],ns_ebook['value'],Literal[result]))
 
-calculate_id = 'urn:uuid' + str(uuid.uuid1())
-store.add((calculate_id,RDF.type,ns_ebook['calculate']))
-store.add((calculate_id,ns_prov['starttime'],Literal[t1]))
-store.add((calculate_id,ns_prov['endtime'],Literal[t2]))
+calculate_uuid = str(uuid.uuid1())
+store.add((ns_ebook[calculate_uuid],RDF.type,ns_ebook['calculate']))
+store.add((ns_ebook[calculate_uuid],ns_prov['starttime'],Literal[t1]))
+store.add((ns_ebook[calculate_uuid],ns_prov['endtime'],Literal[t2]))
 
 ebook_execute = '%s/%s/execution.py' % (ebooks_dir, ebookname)
 store.add((ebook_execute,RDF.type,ns_ebook['input']))
 
-software_id = 'urn:uuid' + str(uuid.uuid1())
+software_uuid = str(uuid.uuid1())
 store.add((software_id,RDF.type,ns_ebook['software']))
 
-store.add((result_id,ns_prov['wasDerivedFrom'],ebook_file_URI))
-store.add((result_id,ns_prov['wasGeneratedBy'],calculate_id))
-store.add((result_id,ns_prov['wasDerivedFrom'],ebook_execute))
-store.add((calculate_id,ns_prov['wasStartedBy'],software_id))
+store.add((ns_ebook[result_uuid],ns_prov['wasDerivedFrom'],ebook_file_URI))
+store.add((ns_ebook[result_uuid],ns_prov['wasGeneratedBy'],ns_ebook[calculate_uuid]))
+store.add((ns_ebook[result_uuid],ns_prov['wasDerivedFrom'],ebook_execute))
+store.add((ns_ebook[result_uuid],ns_prov['wasStartedBy'],ns_ebook[software_uuid]))
 #store.add((server,ns_prov['wasAttributedTo'],software))
 #store.add((software,ns_prov['wasStartedBy'],calculate))
 
